25553c983e78762aab7fd1ed5ffecd336df74b2b
Siarhei Saroka <siarhei_saroka@epam.com>
2022-12-18 23:26:11 +0300

    part1 done.

--- transfer.sh
+++ transfer.sh
@@ -1,28 +1,88 @@
-#!/bin/curl
+#!/bin/bash
 
-currentVersion="0.0.1"
+CURRENT_VERSOIN="0.0.1"
 
-httpSingleUpload()
-{
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+httpSingleUpload() {
+  #response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+  echo "Uploading $1"
+    response=$(curl -A curl -# --upload-file "$1" "https://transfer.sh/$1") || {
+    echo "Failure!"
+    return 1
+  }
+  echo "Transfer File URL: $response"
+
 }
 
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Transfer File URL: $response
+# printUploadResponse() {
+#   fileID=$(echo "$response" | cut -d "/" -f 4)
+#   cat <<EOF
+# Transfer File URL: $response
+# EOF
+# }
+
+# singleUpload() {
+#   filePath=$(echo "$1" | sed s:"~":"$HOME":g)
+#   if ! -f "$filePath"; then {
+#     echo "Error: invalid file path"
+#     return 1
+#   }; fi
+#   tempFileName=$(echo "$1" | sed "s/.*\///")
+#   echo "Uploading $tempFileName"
+#   httpSingleUpload "$filePath $tempFileName"
+# }
+args_upload() {
+  for file in "$@"; do
+    file_name=$(basename "$file")
+    if [[ ! -e "$file" ]]; then
+      echo "$file: No such file"
+    else
+      httpSingleUpload "$file"
+    fi
+  done
+}
+single_download() {
+  echo "Downloading $4"
+  res=$(curl -# --write-out %{http_code} https://transfer.sh/"$3"/"$4" -o "$2")
+  if [[ "res" -ne 200 ]] ; then
+  echo "Download error $res"
+  return 1
+  else echo "Success!"
+  fi
+
+}
+#singleUpload "$1" || exit 1
+#printUploadResponse
+if [ $# -eq 0 ]; then
+  printf "No arguments specified.\nUsage:\ntransfer <file1> <file1> ... <fileN> \ntransfer <file_name>\n" >&2
+  exit 1
+fi
+
+case "${1}" in
+  "-d")
+    args_download "$@"
+  ;;
+  "-h")
+    old_ifs=$IFS
+    IFS='' read -r -d '' hlp_msg <<'EOF'
+    Description: Bash tool to transfer files from the command line.
+    Usage:
+      -d  ...
+      -h  Show the help ... 
+      -v  Get the tool version
+      <file1> <file1> ... <fileN> transfer files <file1> <file1> ... <fileN>
+    Examples:
+    ./transfer.sh <file1> <file1> ... <fileN>
+    ./transfer.sh -d ./test Mij6ca test.txt
 EOF
-}
+  echo "$hlp_msg"
+  IFS=$old_ifs
+  ;;
+  "-v")
+    echo "$CURRENT_VERSOIN"
+  ;;
+  *)args_upload "$@"
+  ;;
+esac
 
-singleUpload()
-{
-  filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if ! -f "$filePath" ;then { echo "Error: invalid file path"; return 1;}; fi
-  tempFileName=$(echo "$1" | sed "s/.*\///")
-  echo "Uploading $tempFileName"
-  httpSingleUpload "$filePath $tempFileName"
-}
 
-singleUpload "$1" || exit 1
-printUploadResponse
+
